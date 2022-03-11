3e35a36ce978ee710df8ef98829c8d29099f2b1a
stressTT <batyrkhanb@bk.ru>
2022-03-12 02:04:52 +0600

    lava + dagon+ trigger

--- scripts/vscripts/lavadmg.lua
+++ scripts/vscripts/lavadmg.lua
@@ -1,43 +1,43 @@
-LinkLuaModifier("modifier_lava_damage","lavadamage.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_lava_damage_1","lavadmg.lua", LUA_MODIFIER_MOTION_NONE)
 function StartTouchDamage( trigger )
     local ent = trigger.activator
 
-    ent:AddNewModifier(ent, self, "modifier_lava_damage", {})
+    ent:AddNewModifier(ent, self, "modifier_lava_damage_1", {})
 end
 
 function EndTouch( trigger )
     local ent = trigger.activator
-    ent:RemoveModifierByName("modifier_lava_damage")
+    ent:RemoveModifierByName("modifier_lava_damage_1")
 end
 
 -----------------------------------------------------------------------------------------
 
-modifier_lava_damage = modifier_lava_damage or class({})
+modifier_lava_damage_1 = modifier_lava_damage_1 or class({})
 
-function modifier_lava_damage:IsHidden()
+function modifier_lava_damage_1:IsHidden()
     return true
 end
 
-function modifier_lava_damage:IsPassive()
+function modifier_lava_damage_1:IsPassive()
     return false
 end
 
-function modifier_lava_damage:IsPurgable()
+function modifier_lava_damage_1:IsPurgable()
     return false
 end
 
-function modifier_lava_damage:OnCreated()
+function modifier_lava_damage_1:OnCreated()
     if not IsServer() then return end
     self:StartIntervalThink( 0.08 )
 end
 
-function modifier_lava_damage:OnIntervalThink()
+function modifier_lava_damage_1:OnIntervalThink()
     if IsServer() then
         local damage_table = {
             victim = self:GetParent(),
             attacker = self:GetParent(),
             ability = self,
-            damage = 5,
+            damage = 10,
             damage_type = DAMAGE_TYPE_PURE
         }
         ApplyDamage(damage_table)
--- scripts/vscripts/lavadmgg.lua
+++ scripts/vscripts/lavadmgg.lua
@@ -1,37 +1,37 @@
-LinkLuaModifier("modifier_lava_damage","lavadamage.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_lava_damage_2","lavadmgg.lua", LUA_MODIFIER_MOTION_NONE)
 function StartTouchDamage( trigger )
     local ent = trigger.activator
 
-    ent:AddNewModifier(ent, self, "modifier_lava_damage", {})
+    ent:AddNewModifier(ent, self, "modifier_lava_damage_2", {})
 end
 
 function EndTouch( trigger )
     local ent = trigger.activator
-    ent:RemoveModifierByName("modifier_lava_damage")
+    ent:RemoveModifierByName("modifier_lava_damage_2")
 end
 
 -----------------------------------------------------------------------------------------
 
-modifier_lava_damage = modifier_lava_damage or class({})
+modifier_lava_damage_2 = modifier_lava_damage_2 or class({})
 
-function modifier_lava_damage:IsHidden()
+function modifier_lava_damage_2:IsHidden()
     return true
 end
 
-function modifier_lava_damage:IsPassive()
+function modifier_lava_damage_2:IsPassive()
     return false
 end
 
-function modifier_lava_damage:IsPurgable()
+function modifier_lava_damage_2:IsPurgable()
     return false
 end
 
-function modifier_lava_damage:OnCreated()
+function modifier_lava_damage_2:OnCreated()
     if not IsServer() then return end
     self:StartIntervalThink( 0.08 )
 end
 
-function modifier_lava_damage:OnIntervalThink()
+function modifier_lava_damage_2:OnIntervalThink()
     if IsServer() then
         local damage_table = {
             victim = self:GetParent(),
